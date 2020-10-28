f0fe034ecf4fcca81626ab99b930b70d5b1e2dd3
Python-lear <24eriebli@athenian.org>
2020-10-28 16:27:59 +0000

    Ct notes

--- my_code.py
+++ my_code.py
@@ -1,3 +1,25 @@
 # This is an example of a comment that Python ignores. Use these to make notes as you write your code. You will also fill out the top comment with any help or resources you use.
 # Collaborators:               (fill this is on every assignment, even if the answer is "none")
+#print("Hello world!")
+#x=5
+#print (x)
+#y=3.14159
+#print(y)
+#name='Ella'
+#print(name)
+#z=True
+#print(type(z))
+#print(type(x))
+#print(type(y))
+#print(type(name))
 
+name='C period'
+print(f'hello,{name}')
+print(f'hi, i am from {name}')
+#user input
+print('what is your name?')
+name=input()
+print(f'hello,{name}')
+print(f'{name}, I am Computer.')
+print('I shall be your assistant and know things about you that a computer should not know.')
+print('Just kidding.')
